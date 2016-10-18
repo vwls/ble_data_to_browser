ac126808ff58acc67092311986a135a921b53c3a
lifeonhoth <fhbach@gmail.com>
2016-10-18 16:35:40 -0400

    adding comments to main.js

--- public/main.js
+++ public/main.js
@@ -1,5 +1,11 @@
 var socket = io.connect('http://localhost:3000');
 socket.on('bluetooth-data', function(data) {
 
+
+/*
+LET'S PUT ALL THE HEAVY LIFTING CODE IN HERE:
+set the div to display incoming data: document.getElementsById("readings").innerHTML = data;
+
+*/
 	
 });
