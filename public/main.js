b7827a8bf617d002b26eda25ceb389b41ef64be0
lifeonhoth <fhbach@gmail.com>
2016-10-25 12:29:40 -0400

    adding node modules

--- public/main.js
+++ public/main.js
@@ -4,7 +4,14 @@ socket.on('bluetooth-data', function(data) {
 
 /*
 LET'S PUT ALL THE HEAVY LIFTING CODE IN HERE:
+
 set the div to display incoming data: document.getElementsById("readings").innerHTML = data;
+
+function testFunction(){
+	if (pressureDiff > threshold){
+		pressureDiffText.color = #0000
+	}
+}
 
 */
 	
